df46ae58dea541628054788105959633a1509292
giordi fungula <jodi.fungula@gmail.com>
2021-12-24 06:40:54 +0200

    about base image and info added

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import profileImage from 'images/myself.jpeg';
+import giphy from 'images/animated.gif';
+import { aboutMeInfo } from 'data/aboutMeInfo';
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    // TODO: to add animation to about page
+    <div className="container mx-auto">
+      <div className="block sm:flex sm:gap-10 mt-5 sm:mt-20">
+        {/* About me portfolio image start */}
+        <div className="w-full sm:w-1/4 mb-7 sm:mb-0">
+          <img src={profileImage} className="rounded-lg w-96" alt="" />
+        </div>
+        {/* Add Giphy here */}
+
+        {/* TODO to add toggle switch to select different image  */}
+
+        {/* About me details start */}
+        <div className="w-full sm:w-3/4 text-left">
+          {aboutMeInfo.map((bio) => (
+            <p
+              className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+              key={bio.id}
+            >
+              {bio.bio}
+            </p>
+          ))}
+        </div>
+        {/* About me BIO end */}
+      </div>
     </div>
   );
 };
---
+++ src/data/aboutMeInfo.ts
@@ -0,0 +1,23 @@
+interface AboutInfo {
+  id: number;
+  bio: string;
+}
+
+export const aboutMeInfo = [
+  {
+    id: 1,
+    bio: 'I am an individual who is fascinated with learning. I am passionately interested in Education, People, and Tech. My passion for education and learning makes me an active listener, a curious individual, and inevitably a lifelong learner. I see myself as a self-starter and a driven learner, also enjoying a stimulating conversation, a strongly supported argument. I enjoy the challenge of problem solving, analytical thinking , being an innovative learner, and a versatile athlete. My interests are also hiking, good music, football and most importantly revamping my baking and cooking skills.'
+  },
+  {
+    id: 2,
+    bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?'
+  },
+  {
+    id: 3,
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quis.'
+  },
+  {
+    id: 4,
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quis.'
+  }
+] as AboutInfo[];
