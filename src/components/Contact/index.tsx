df29711a59900c5450ba793555af73687888ab1d
giordi fungula <jodi.fungula@gmail.com>
2021-12-25 23:08:22 +0200

    updates to the contact form

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,25 +9,25 @@ export default function Contact() {
           Contact
         </h1>
       </div>
-      <div className="relative  rounded-md shadow-md bg-[#02044A] p-4 md:p-10 lg:p-25 max-w-6xl mx-auto mb-20 mt-0">
+      <div className="relative  rounded-md shadow-md bg-indigo-500   p-4 md:p-10 lg:p-25 max-w-6xl mx-auto mb-20 mt-0">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="md:ml-4">
             <header className="">
-              <h1 className="text-gray-50 font-semibold text-2xl">
+              <h1 className="  font-semibold text-2xl text-black dark:text-ternary-light">
                 Get in touch, let's talk.
               </h1>
-              <p className="font-light text-base text-gray-200 mt-2">
+              <p className="font-light text-base text-black dark:text-ternary-light  mt-2">
                 Fill in the details and I'll get back to you as soon as I can.
               </p>
             </header>
             <div className="icons-container inline-flex flex-col my-20">
-              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+              <div className=" mb-5 flex flex-row items-center space-x-6 rounded-md border border-blue-500 hover:border hover:border-white  p-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-telephone-fill h-4 w-4 text-blue-500"
+                  className="bi bi-telephone-fill h-4 w-4 text-primary-dark"
                   viewBox="0 0 16 16"
                 >
                   <path
@@ -39,13 +39,13 @@ export default function Contact() {
                   {userData.phone}
                 </p>
               </div>
-              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+              <div className="mb-5 flex flex-row items-center space-x-6 rounded-md border border-blue-500 hover:border hover:border-white p-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-envelope-fill h-4 w-4 text-blue-500"
+                  className="bi bi-envelope-fill h-4 w-4 text-primary-dark"
                   viewBox="0 0 16 16"
                 >
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
@@ -54,13 +54,13 @@ export default function Contact() {
                   {userData.email}
                 </p>
               </div>
-              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-blue-500 hover:border hover:border-white p-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-pin-fill h-4 w-4 text-blue-500"
+                  className="bi bi-pin-fill h-4 w-4 text-primary-dark"
                   viewBox="0 0 16 16"
                 >
                   <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
